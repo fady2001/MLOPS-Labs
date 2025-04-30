3b83ceb8a45a297410fdb1eff91d6124e8010a73
Fady Adel <fady.adel2001@gmail.com>
2025-04-30 17:22:52 +0300

    dataset loader and splitter

--- lab0/src/dataset.py
+++ lab0/src/dataset.py
@@ -1,29 +1,42 @@
-from pathlib import Path
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-from loguru import logger
-from tqdm import tqdm
-import typer
-
-from src.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
-
-app = typer.Typer()
+from config import RAW_DATA_DIR
+from logger import ExecutorLogger
 
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+class DatasetLoader:
+    
+    @staticmethod
+    def load_dataset(filename: str, id_col:str, logger:ExecutorLogger) -> pd.DataFrame:
+        """
+        Read a dataset from a CSV file.
+        """
+        filepath = RAW_DATA_DIR / filename
+        
+        if not filepath.exists() or not filepath.is_file():
+            logger.error(f"File {filepath} does not exist or is not a file.")
+            raise FileNotFoundError(f"File {filepath} does not exist or is not a file.")
+        df = pd.read_csv(filepath, sep=",", header=0, index_col=0)
+        df.columns = df.columns.str.strip()
+        df.set_index(id_col, inplace=True)
+        logger.success(f"Dataset loaded from {filepath}.")
+        return df
 
+    @staticmethod
+    def split_dataset(df: pd.DataFrame, logger: ExecutorLogger, train_size: float = 0.8) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Split a dataset into training and testing sets.
+        """
+        if not 0 < train_size < 1:
+            logger.error("train_size must be between 0 and 1.")
+            raise ValueError("train_size must be between 0 and 1.")
+        if df.empty:
+            logger.error("train_size must be between 0 and 1.")
+            raise ValueError("DataFrame is empty.")
 
-if __name__ == "__main__":
-    app()
+        train_df, test_df = train_test_split(df, train_size=train_size, random_state=42)
+        logger.success(f"Dataset split into training and testing sets with train size {train_size}.")
+        logger.info(f"Training set size: {len(train_df)}")
+        logger.info(f"Testing set size: {len(test_df)}")
+        return train_df, test_df
