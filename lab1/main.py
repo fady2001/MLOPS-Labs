c7ff07b5a00849fbea7f1e6a146ecd09575ef909
Fady Adel <fady.adel2001@gmail.com>
2025-05-01 15:16:12 +0300

    lab2

--- lab1/main.py
+++ lab1/main.py
@@ -1,3 +1,5 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
 from sklearn.ensemble import RandomForestClassifier
 
 from src.dataset.dataset import Dataset
@@ -7,27 +9,23 @@ from src.modeling.training import train
 from src.utils import save_components
 
 
-def main(logger:ExecutorLogger) -> None:
+@hydra.main(config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    logger = ExecutorLogger("training")
     logger.info("Training started")
-    train_ds = Dataset(
-        filename="train.csv",
-        id_col="PassengerId",
-        target_col="Survived",
-        logger=logger
-    )
+    logger.info("Pipeline Parameters: \n" f"{OmegaConf.to_yaml(cfg)}")
+    train_ds = Dataset(cfg.pipeline.data,logger=logger)
     train_ds.engineer_features()
     train_ds.preprocess_train()
     model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
     X_train, y_train, X_val, y_val = train_ds.get_X_train_val_y_train_val()
     train(model, X_train,y_train, logger)
-    evaluate(X_val, y_val, model_name="random_forest", logger=logger)
-    save_components(model, model_name="random_forest", ds=train_ds, encoder_name="label_encoders", logger=logger)
+    evaluate(X_val, y_val, cfg=cfg.pipeline.evaluate, logger=logger)
+    save_components(model, model_name=cfg.pipeline.model.model_name, ds=train_ds, encoder_name=cfg.pipeline.save.encoder_name, logger=logger)
     logger.info("Training finished")
     
     test_df = Dataset(
-        filename="test.csv",
-        id_col="PassengerId",
-        target_col=None,
+        cfg=cfg.pipeline.test,
         logger=logger,
         encoder=train_ds.encoders,
         scaler=train_ds.scalers
@@ -35,8 +33,7 @@ def main(logger:ExecutorLogger) -> None:
     test_df.engineer_features()
     test_df.preprocess_test()
     X_test, test_id = test_df.get_test()
-    generate_submission_file('random_forest',X_test, test_id , logger=logger)
+    generate_submission_file(X_test, test_id, cfg=cfg.pipeline.evaluate, logger=logger)
 
 if __name__ == "__main__":
-    logger = ExecutorLogger("training")
-    main(logger)
+    main()
--- lab1/src/dataset/dataset.py
+++ lab1/src/dataset/dataset.py
@@ -2,38 +2,37 @@ import os
 import pickle
 from typing import Tuple
 
+from omegaconf import DictConfig
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from src.config import INTERIM_DATA_DIR, MODELS_DIR, PROCESSED_DATA_DIR, RAW_DATA_DIR
 from src.dataset.features import extract_features
 from src.dataset.preprocess import after_split_preprocess, before_split_preprocess, process_test
 from src.logger import ExecutorLogger
 
 
 class Dataset:
-    def __init__(self, filename: str, id_col: str, target_col:str,logger: ExecutorLogger,encoder = None, scaler = None) -> None:
+    def __init__(self, cfg: DictConfig,logger: ExecutorLogger,encoder = None, scaler = None) -> None:
         """
         Initialize the Dataset class.
         """
-        self.filename:str = filename
-        self.id_col:str = id_col
-        self.target_col:str = target_col
+        self.cfg:DictConfig = cfg
         self.logger:ExecutorLogger = logger
-        self.df:pd.DataFrame = self.load_dataset(filename, id_col, logger)
+        self.df:pd.DataFrame = self.load_dataset(cfg.file_name, cfg.id_col)
         self.encoders = encoder
         self.scalers = scaler
-        self.test_id = self.df[self.id_col]
+        self.test_id = self.df[cfg.id_col]
         
-    def load_dataset(self,filename: str, id_col:str, logger:ExecutorLogger) -> pd.DataFrame:
+    def load_dataset(self,filename: str, id_col:str) -> pd.DataFrame:
         """
         Read a dataset from a CSV file.
         """
-        filepath = RAW_DATA_DIR / filename
-        
-        if not filepath.exists() or not filepath.is_file():
-            self.logger.error(f"File {filepath} does not exist or is not a file.")
-            raise FileNotFoundError(f"File {filepath} does not exist or is not a file.")
+        print(os.getcwd())
+        filepath = os.path.join(self.cfg.raw_data_path,filename)
+        print(filepath)
+        if not os.path.exists(filepath):
+            self.logger.error(f"File {filepath} not found.")
+            raise FileNotFoundError(f"File {filepath} not found.")
         df = pd.read_csv(filepath, sep=",")
         if id_col not in df.columns:
             self.logger.error(f"Column {id_col} not found in the dataset.")
@@ -45,7 +44,7 @@ class Dataset:
         """
         Split a dataset into training and testing sets.
         """
-        train_df, test_df = train_test_split(df, test_size=test_ratio, random_state=42)
+        train_df, test_df = train_test_split(df, test_size=test_ratio, random_state=self.cfg.random_state)
         self.logger.success(f"Dataset split into training and testing sets with test size {test_ratio}.")
         self.logger.info(f"Training set size: {len(train_df)}")
         self.logger.info(f"Testing set size: {len(test_df)}")
@@ -56,19 +55,19 @@ class Dataset:
         Perform feature engineering on the dataset.
         """
         self.df = extract_features(self.df)
-        self.save_dataset(self.df, filename=INTERIM_DATA_DIR / self.filename)
+        self.save_dataset(self.df, filename=self.cfg.file_name, dir=self.cfg.interim_data_path)
         return self.df
     
     def preprocess_train(self) -> None:
         # drop column with more than 100 unique values
-        self.df = self.df.loc[:,self.df.nunique() < 15]        
+        self.df = self.df.loc[:,self.df.nunique() < 15]
         preprocessed_df,self.encoders = before_split_preprocess(self.df)
-        self.save_dataset(preprocessed_df, filename="train.csv", dir=PROCESSED_DATA_DIR)
-        self.train_df, self.val_df = self.split_dataset(preprocessed_df)
-        preprocessed_train_df,self.scalers = after_split_preprocess(self.train_df, self.target_col)
-        preprocessed_val_df = process_test(self.val_df,self.target_col, self.encoders, self.scalers)
-        self.save_dataset(preprocessed_train_df, filename="train.csv", dir=PROCESSED_DATA_DIR)
-        self.save_dataset(preprocessed_val_df, filename="val.csv", dir=PROCESSED_DATA_DIR)
+        self.save_dataset(preprocessed_df, filename=self.cfg.file_name, dir=self.cfg.processed_data_path)
+        self.train_df, self.val_df = self.split_dataset(preprocessed_df, self.cfg.test_size)
+        preprocessed_train_df,self.scalers = after_split_preprocess(self.train_df, self.cfg.target_col)
+        preprocessed_val_df = process_test(self.val_df,self.cfg.target_col, self.encoders, self.scalers)
+        self.save_dataset(preprocessed_train_df, filename=self.cfg.file_name, dir=self.cfg.processed_data_path)
+        self.save_dataset(preprocessed_val_df, filename=self.cfg.val_name, dir=self.cfg.processed_data_path)
         
     def preprocess_test(self) -> pd.DataFrame:
         """
@@ -79,15 +78,15 @@ class Dataset:
         if self.encoders is None or self.scalers is None:
             self.logger.error("Encoders and scalers are not available. Please preprocess the training dataset first.")
             raise ValueError("Encoders and scalers are not available. Please preprocess the training dataset first.")
-        preprocessed_df = process_test(self.df, self.target_col,self.encoders, self.scalers)
-        self.save_dataset(preprocessed_df, filename="test.csv", dir=PROCESSED_DATA_DIR)
+        preprocessed_df = process_test(self.df, self.cfg.target_col,self.encoders, self.scalers)
+        self.save_dataset(preprocessed_df, filename=self.cfg.file_name, dir=self.cfg.processed_data_path)
         return preprocessed_df
         
-    def save_dataset(self, df: pd.DataFrame, filename: str, dir:str=PROCESSED_DATA_DIR) -> None:
+    def save_dataset(self, df: pd.DataFrame, filename: str,dir:str) -> None:
         """
         Save a DataFrame to a CSV file.
         """
-        filepath = dir / filename
+        filepath = os.path.join(dir,filename)
         df.to_csv(filepath, sep=",")
         self.logger.success(f"Dataset saved to {filepath}.")
         self.logger.info(f"Dataset shape: {df.shape}")
@@ -96,13 +95,13 @@ class Dataset:
         """
         Get the features and target variable from the dataset.
         """
-        if self.target_col not in self.df.columns:
-            self.logger.error(f"Target column {self.target_col} not found in the dataset.")
-            raise ValueError(f"Target column {self.target_col} not found in the dataset.")
-        X_train = self.train_df.drop(columns=[self.target_col])
-        y_train = self.train_df[self.target_col]
-        X_val = self.val_df.drop(columns=[self.target_col])
-        y_val = self.val_df[self.target_col]
+        if self.cfg.target_col not in self.df.columns:
+            self.logger.error(f"Target column {self.cfg.target_col} not found in the dataset.")
+            raise ValueError(f"Target column {self.cfg.target_col} not found in the dataset.")
+        X_train = self.train_df.drop(columns=[self.cfg.target_col])
+        y_train = self.train_df[self.cfg.target_col]
+        X_val = self.val_df.drop(columns=[self.cfg.target_col])
+        y_val = self.val_df[self.cfg.target_col]
         return X_train, y_train,X_val, y_val
     
     def get_test(self) -> Tuple[pd.DataFrame,pd.Series]:
@@ -116,7 +115,7 @@ class Dataset:
         """
         Save the encoders to a file.
         """
-        model_path = os.path.join(MODELS_DIR, model_name)
+        model_path = os.path.join(self.cfg.model_path, model_name)
         if not os.path.exists(model_path):
             os.makedirs(model_path)
         with open(os.path.join(model_path, f"{encoder_name}.pkl"), "wb") as pkl:
@@ -126,7 +125,7 @@ class Dataset:
         """
         Save the scalers to a file.
         """
-        model_path = os.path.join(MODELS_DIR, model_name)
+        model_path = os.path.join(self.cfg.model_path, model_name)
         if not os.path.exists(model_path):
             os.makedirs(model_path)
         with open(os.path.join(model_path, "scaler.pkl"), "wb") as pkl:
