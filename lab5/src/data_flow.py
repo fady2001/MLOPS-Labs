0cabb39cc5325188a55bdee1fccc058b5beaac59
Fady Adel <fady.adel2001@gmail.com>
2025-05-17 23:33:57 +0300

    fetch data from api and load it to motherduck

--- lab5/src/data_flow.py
+++ lab5/src/data_flow.py
@@ -1,12 +1,15 @@
 import datetime
+import os
 
+from dotenv import load_dotenv
 import duckdb
 import pandas as pd
 from prefect import flow, task
 from prefect.tasks import task_input_hash
 import requests
 
-from .data_models import APIData, URLParams
+from data_models import APIData, URLParams
+from globals import logger
 
 
 @task(
@@ -18,25 +21,29 @@ from .data_models import APIData, URLParams
     timeout_seconds=60,
     log_prints=True,
 )
-def get_api_data(data_url: str, params: URLParams, logger) -> APIData:
+def get_api_data(data_url: str, params: URLParams) -> APIData:
     logger.info("Retrieving weather data from API...")
     response = requests.get(url=data_url, params=params)
-
     if response.status_code == 200:
         logger.info(f"Data retrieved successfully (status code: {response.status_code})")
         data = response.json()
-        timestamps = data.get("hourly", {}).get("time", [])
-        temps = data.get("hourly", {}).get("temperature_2m", [])
-        humedity = data.get("hourly", {}).get("relative_humidity_2m", [])
-        rain = data.get("hourly", {}).get("rain", [])
-        precipitation = data.get("hourly", {}).get("precipitation", [])
-        cloud_cover = data.get("hourly", {}).get("cloud_cover", [])
-
-        if len(temps) >= 24 and None not in temps:
-            logger.info("Data is complete and valid.")
-            return data
-        else:
-            raise ValueError("Retrieved data is incomplete or contains missing values.")
+        required_keys = [
+            "time",
+            "temperature_2m",
+            "relative_humidity_2m",
+            "rain",
+            "precipitation",
+            "cloud_cover",
+            "wind_speed_10m",
+        ]
+        if not all(key in data["hourly"] for key in required_keys):
+            raise KeyError("Missing required keys in the API response.")
+        for key in required_keys:
+            if len(data["hourly"][key]) >= 24 and None not in data["hourly"][key]:
+                logger.info("Data is complete and valid.")
+            else:
+                raise ValueError(f"Missing values in {key} data.")
+        return data
     else:
         raise ConnectionError(f"Failed to retrieve data (status code: {response.status_code})")
 
@@ -50,21 +57,27 @@ def get_api_data(data_url: str, params: URLParams, logger) -> APIData:
     timeout_seconds=60,
     log_prints=True,
 )
-def transform_api_data(data: APIData, logger) -> pd.DataFrame:
+def transform_api_data(data: APIData) -> pd.DataFrame:
     logger.info("Transforming API data into structured DataFrame...")
     timestamps = data["hourly"]["time"]
     temperatures = data["hourly"]["temperature_2m"]
-    timezone = data["timezone"]
+    relative_humidity = data["hourly"]["relative_humidity_2m"]
+    rain = data["hourly"]["rain"]
+    precipitation = data["hourly"]["precipitation"]
+    cloud_cover = data["hourly"]["cloud_cover"]
+    wind_speed = data["hourly"]["wind_speed_10m"]
 
     df = pd.DataFrame(
         {
-            "location_id": [75354428] * len(timestamps),
-            "reading_timestamp": pd.to_datetime(timestamps),
+            "timestamp": pd.to_datetime(timestamps),
             "temperature": temperatures,
-            "tz": [timezone] * len(timestamps),
+            "relative_humidity": relative_humidity,
+            "rain": rain,
+            "precipitation": precipitation,
+            "cloud_cover": cloud_cover,
+            "wind_speed": wind_speed,
         }
     )
-
     logger.info("Transformation complete. Data is ready for loading.")
     return df
 
@@ -78,7 +91,7 @@ def transform_api_data(data: APIData, logger) -> pd.DataFrame:
     timeout_seconds=120,
     log_prints=True,
 )
-def check_if_data_exists(db_conn, running_dt: str, logger) -> None:
+def check_if_data_exists(db_conn, running_dt: str) -> None:
     """
     Check if weather data for a specific date exists in the DB, and delete it if found.
 
@@ -90,10 +103,10 @@ def check_if_data_exists(db_conn, running_dt: str, logger) -> None:
         Date to check in YYYY-MM-DD format.
     """
     logger.info(f"Checking for existing data for date: {running_dt}")
-    db_conn.sql("USE ml_apps")
+    db_conn.sql("USE weather_data")
     query = f"""
-        SELECT * FROM ml_apps.iti_weather_forecasting.hourly_weather_data 
-        WHERE strftime(reading_timestamp, '%Y-%m-%d') = '{running_dt}'
+        SELECT * FROM weather_data.hourly_weather_data 
+        WHERE strftime(timestamp, '%Y-%m-%d') = '{running_dt}'
     """
     result_df = db_conn.sql(query).df()
     logger.info(f"Found {len(result_df)} existing records.")
@@ -101,8 +114,8 @@ def check_if_data_exists(db_conn, running_dt: str, logger) -> None:
     if not result_df.empty:
         logger.info("Data exists. Deleting previous records...")
         db_conn.sql(f"""
-            DELETE FROM ml_apps.iti_weather_forecasting.hourly_weather_data 
-            WHERE strftime(reading_timestamp, '%Y-%m-%d') = '{running_dt}'
+            DELETE FROM weather_data.hourly_weather_data 
+            WHERE strftime(timestamp, '%Y-%m-%d') = '{running_dt}'
         """)
         logger.info("Old data deleted successfully.")
 
@@ -116,7 +129,7 @@ def check_if_data_exists(db_conn, running_dt: str, logger) -> None:
     timeout_seconds=120,
     log_prints=True,
 )
-def load_to_motherduck(df: pd.DataFrame, db_conn, logger) -> None:
+def load_to_motherduck(df: pd.DataFrame, db_conn) -> None:
     """
     Load a weather DataFrame into the MotherDuck database.
 
@@ -128,8 +141,8 @@ def load_to_motherduck(df: pd.DataFrame, db_conn, logger) -> None:
         MotherDuck database connection.
     """
     logger.info("Inserting data into MotherDuck...")
-    db_conn.sql("USE ml_apps")
-    db_conn.sql("INSERT INTO ml_apps.iti_weather_forecasting.hourly_weather_data SELECT * FROM df")
+    db_conn.sql("USE weather_data")
+    db_conn.sql("INSERT INTO weather_data.hourly_weather_data SELECT * FROM df")
     logger.info("Data inserted successfully.")
 
 
@@ -142,7 +155,7 @@ def load_to_motherduck(df: pd.DataFrame, db_conn, logger) -> None:
     timeout_seconds=120,
     log_prints=True,
 )
-def delete_out_of_range_data(db_conn, thresh_dt: str, logger) -> None:
+def delete_out_of_range_data(db_conn, thresh_dt: str) -> None:
     """
     Delete weather data older than 800 days before the threshold date.
 
@@ -154,10 +167,10 @@ def delete_out_of_range_data(db_conn, thresh_dt: str, logger) -> None:
         Threshold date in YYYY-MM-DD format.
     """
     logger.info("Cleaning up old data...")
-    db_conn.sql("USE ml_apps")
+    db_conn.sql("USE weather_data")
     db_conn.sql(f"""
-        DELETE FROM ml_apps.iti_weather_forecasting.hourly_weather_data 
-        WHERE reading_timestamp <= CAST('{thresh_dt}' AS DATE) - 800
+        DELETE FROM weather_data.hourly_weather_data 
+        WHERE timestamp <= CAST('{thresh_dt}' AS DATE) - 800
     """)
     logger.info("Old records deleted successfully.")
 
@@ -189,14 +202,44 @@ def data_flow(
     deleting_thresh_dt : str
         Date used as a threshold for data deletion (YYYY-MM-DD).
     """
-    api_data = get_api_data(data_url=api_data_url, params=url_params, logger=logger)
-    transformed_df = transform_api_data(data=api_data, logger=logger)
+    api_data = get_api_data(data_url=api_data_url, params=url_params)
+    transformed_df = transform_api_data(data=api_data)
 
     logger.info("Establishing connection to MotherDuck...")
-    with duckdb.connect(f"md:?motherduck_token={db_token}") as conn:
-        logger.info("Connected to MotherDuck successfully.")
-        check_if_data_exists(db_conn=conn, running_dt=deleting_thresh_dt, logger=logger)
-        load_to_motherduck(df=transformed_df, db_conn=conn, logger=logger)
-        delete_out_of_range_data(db_conn=conn, thresh_dt=deleting_thresh_dt, logger=logger)
+    conn = duckdb.connect(f"md:?motherduck_token={db_token}")
+    if conn is None:
+        logger.error("Failed to connect to MotherDuck.")
+        raise ConnectionError("Connection to MotherDuck failed.")
+    if conn:
+        logger.info("Connection to MotherDuck established successfully.")
+    logger.info("Connected to MotherDuck successfully.")
+    check_if_data_exists(db_conn=conn, running_dt=deleting_thresh_dt)
+    load_to_motherduck(df=transformed_df, db_conn=conn)
+    delete_out_of_range_data(db_conn=conn, thresh_dt=deleting_thresh_dt)
 
     logger.info("Pipeline completed and connection closed.")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    data_flow(
+        api_data_url="https://archive-api.open-meteo.com/v1/archive",
+        url_params=URLParams(
+            latitude=27,
+            longitude=30,
+            start_date="2025-04-29",
+            end_date="2025-04-30",
+            hourly=[
+                "temperature_2m",
+                "relative_humidity_2m",
+                "rain",
+                "precipitation",
+                "cloud_cover",
+                "wind_speed_10m",
+            ],
+            timezone="Africa/Cairo",
+        ),
+        db_token=os.getenv("MOTHERDUCK_TOKEN"),
+        deleting_thresh_dt="2023-10-01",
+        logger=logger,
+    )
