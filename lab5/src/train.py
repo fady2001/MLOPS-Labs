a0a4503c07be7f93f142892ebeb6ef260f036a0f
Fady Adel <fady.adel2001@gmail.com>
2025-05-18 23:17:31 +0300

    modeling

---
+++ lab5/src/train.py
@@ -0,0 +1,166 @@
+import datetime
+import pickle
+
+import duckdb
+import pandas as pd
+from prefect import flow, task
+from prefect.tasks import task_input_hash
+from sktime.exceptions import NotFittedError
+from sktime.forecasting.fbprophet import Prophet
+
+
+@task(
+    name="GetInferenceData",
+    description="get daily weather data that are needed to forecast weather",
+    tags=["Get", "InferenceData"],
+    cache_key_fn=task_input_hash,
+    cache_expiration=datetime.timedelta(minutes=10),
+    retry_delay_seconds=30,
+    retries=3,
+    log_prints=True,
+    timeout_seconds=60,
+)
+def get_inference_data(conn, running_date: str) -> pd.DataFrame:
+    """connect to motherduck,
+    and get data needed to forecast next 30 days
+
+    Parameters
+    ----------
+    conn : MotherDuck Database Connection
+    running_dt: str
+         string format of pipeline running date
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe of daily historical weather data.
+    """
+    df = conn.sql(
+        "SELECT * FROM ml_apps.iti_weather_forecasting.daily_weather_data WHERE"
+        f" reading_date >= CAST('{running_date}' AS DATE) - INTERVAL '400 days'"
+    ).df()
+    return df
+
+
+@task(
+    name="ForecastWeather",
+    description="Forecast the next 30",
+    tags=["Forecast", "Inference"],
+    retry_delay_seconds=30,
+    retries=3,
+    log_prints=True,
+    timeout_seconds=30,
+)
+def forecast_weather(
+    hist_df: pd.DataFrame, running_date: str
+) -> pd.DataFrame:
+    """forecast the temperature next 30 days
+
+    Parameters
+    ----------
+    hist_df : pd.DataFrame
+        historical temperature dataframe
+    running_date: str
+        string format of pipeline running date
+
+    Returns
+    -------
+    pd.DataFrame
+       dataframe of forecasted temperature
+
+    Raises
+    ------
+    NotFittedError
+        Exception class to raise if estimator is used before fitting
+    """
+    id = pd.Series([75354428 for _ in range(30)], name="location_id")
+    inference_date = pd.Series([running_date for _ in range(30)], name="inference_date")
+    scoring_df = hist_df[["reading_date", "temperature"]]
+    scoring_df.set_index("reading_date", inplace=True)
+    model = Prophet(
+        seasonality_mode="multiplicative",
+        yearly_seasonality=True,
+        weekly_seasonality=True,
+        daily_seasonality=False,
+        interval_width=0.95,
+    )
+    model.fit(scoring_df)
+    try:
+        model.check_is_fitted()
+        preds = model.predict(
+            fh=range(1, 31)
+        )
+        preds.reset_index(inplace=True)
+        preds.columns = ["reading_date", "forecasted_temperature"]
+        preds = pd.concat([id, preds, inference_date], axis=1)
+        return preds
+    except NotFittedError:
+        raise NotFittedError("Loaded Model isn't fitted on Training Data")
+
+
+@task(
+    name="LoadForecastsIntoMotherDuck",
+    description="Load Weather Forecasts into database",
+    tags=["Load", "ForecastedData", "Database"],
+    retry_delay_seconds=30,
+    retries=3,
+    log_prints=True,
+    timeout_seconds=60,
+)
+def load_forecasts_into_db(conn, preds_df: pd.DataFrame) -> None:
+    """load forecasted temperature to motherduck
+
+    Parameters
+    ----------
+    conn : MotherDuck Database Connection
+    preds_df : pd.DataFrame
+        dataframe of forecasted temperature
+    """
+    conn.sql(
+        "INSERT INTO ml_apps.iti_weather_forecasting.daily_forecasted_weather SELECT * FROM"
+        " preds_df"
+    )
+
+
+def delete_out_of_range_data(conn, thresh_date: str, logger) -> None:
+    logger.info("Deleting Out of Range Data")
+    conn.sql(f"""
+            DELETE FROM ml_apps.iti_weather_forecasting.daily_forecasted_weather
+            WHERE inference_date <= CAST('{thresh_date}' AS DATE)-1000
+        """)
+    logger.info("Data Deleted Successfully")
+
+
+@flow(
+    name="WeatherForecastingFlow",
+    description="Forecast Weather, and insert results into DB",
+    validate_parameters=True,
+    log_prints=True,
+)
+def forecast_flow(db_token: str, date: str, logger) -> None:
+    """flow of inference
+
+    Parameters
+    ----------
+    db_token : str
+        MotherDuck Database Credentials
+    date : str
+        running date of the process
+    model_path : str
+        path of pickle file
+    """
+    logger.info("Connecting To MotherDuck to Get/Load Data")
+    with duckdb.connect(f"md:?motherduck_token={db_token}") as conn:
+        logger.info("Getting Scoring Data From MotherDuck")
+        df = get_inference_data(conn=conn, running_date=date)
+        if len(df) > 0:
+            preds = forecast_weather(
+                hist_df=df, running_date=date
+            )
+            logger.info(f"Model Forecasted Next {len(preds)} days")
+            load_forecasts_into_db(conn=conn, preds_df=preds)
+            logger.info("Data Loaded into MotherDuck")
+            delete_out_of_range_data(conn=conn, thresh_date=date, logger=logger)
+        else:
+            logger.info("No Records in Scoring data..")
+    logger.info("Connection with MotherDuck Closed")
